8ae3b3f497178676deb2f09b2d798245a343e8d1
Justin Daniels <JuDaniels@ashleyfurniture.com>
2021-03-18 14:50:38 -0400

    initial commit

---
+++ QueryProducts-NET472/Models/Products.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace QueryProducts_NET472.Models
+{
+    public class Products
+    {
+        [JsonProperty("links")]
+        public List<Link> Links { get; set; }
+
+        [JsonProperty("metadata")]
+        public Metadata Metadata { get; set; }
+
+        [JsonProperty("entities")]
+        public List<Entity> Entities { get; set; }
+    }
+
+    public class Entity
+    {
+        [JsonProperty("additionalDimensions")]
+        public List<AdditionalDimension> AdditionalDimensions { get; set; }
+
+        [JsonProperty("brandName")]
+        public string BrandName { get; set; }
+
+        [JsonProperty("cartonDepthInches")]
+        public string CartonDepthInches { get; set; }
+
+        [JsonProperty("cartonDepthMm")]
+        public string CartonDepthMm { get; set; }
+
+        [JsonProperty("cartonHeightInches")]
+        public string CartonHeightInches { get; set; }
+
+        [JsonProperty("cartonHeightMm")]
+        public string CartonHeightMm { get; set; }
+
+        [JsonProperty("cartonVolumeCuFeet")]
+        public string CartonVolumeCuFeet { get; set; }
+
+        [JsonProperty("cartonVolumeCuMeters")]
+        public string CartonVolumeCuMeters { get; set; }
+
+        [JsonProperty("cartonWidthInches")]
+        public string CartonWidthInches { get; set; }
+
+        [JsonProperty("cartonWidthMm")]
+        public string CartonWidthMm { get; set; }
+
+        [JsonProperty("chairQtyPerCarton")]
+        public string ChairQtyPerCarton { get; set; }
+
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("covers")]
+        public List<Cover> Covers { get; set; }
+
+        [JsonProperty("detailedDescription")]
+        public string DetailedDescription { get; set; }
+
+        [JsonProperty("fobBasePrice")]
+        public string FobBasePrice { get; set; }
+
+        [JsonProperty("friendlyDescription")]
+        public string FriendlyDescription { get; set; }
+
+        [JsonProperty("generalColor")]
+        public List<string> GeneralColor { get; set; }
+
+        [JsonProperty("harmonizationCode")]
+        public string HarmonizationCode { get; set; }
+
+        [JsonProperty("imageSet")]
+        public List<ImageSet> ImageSet { get; set; }
+
+        [JsonProperty("intendedRooms")]
+        public List<string> IntendedRooms { get; set; }
+
+        [JsonProperty("isExpressShipEligible")]
+        public bool IsExpressShipEligible { get; set; }
+
+        [JsonProperty("isSupplierDirectShipOnly")]
+        public bool IsSupplierDirectShipOnly { get; set; }
+
+        [JsonProperty("itemClass")]
+        public string ItemClass { get; set; }
+
+        [JsonProperty("itemCode")]
+        public string ItemCode { get; set; }
+
+        [JsonProperty("itemName")]
+        public string ItemName { get; set; }
+
+        [JsonProperty("itemsPerCase")]
+        public string ItemsPerCase { get; set; }
+
+        [JsonProperty("itemRoomImage")]
+        public Uri ItemRoomImage { get; set; }
+
+        [JsonProperty("itemType")]
+        public string ItemType { get; set; }
+
+        [JsonProperty("itemWeightKg")]
+        public string ItemWeightKg { get; set; }
+
+        [JsonProperty("itemWeightLbs")]
+        public string ItemWeightLbs { get; set; }
+
+        [JsonProperty("isCommodity")]
+        public bool IsCommodity { get; set; }
+
+        [JsonProperty("kitIncludes")]
+        public List<object> KitIncludes { get; set; }
+
+        [JsonProperty("knockout")]
+        public Uri Knockout { get; set; }
+
+        [JsonProperty("largeImageUrl")]
+        public Uri LargeImageUrl { get; set; }
+
+        [JsonProperty("lifestyle")]
+        public List<string> Lifestyle { get; set; }
+
+        [JsonProperty("links")]
+        public List<Link> Links { get; set; }
+
+        [JsonProperty("manufacturerWarrantyDays")]
+        public string ManufacturerWarrantyDays { get; set; }
+
+        [JsonProperty("material")]
+        public List<string> Material { get; set; }
+
+        [JsonProperty("mediumImageUrl")]
+        public Uri MediumImageUrl { get; set; }
+
+        [JsonProperty("numberofPieces", NullValueHandling = NullValueHandling.Ignore)]
+        public string NumberofPieces { get; set; }
+
+        [JsonProperty("pattern")]
+        public List<string> Pattern { get; set; }
+
+        [JsonProperty("pillowSize", NullValueHandling = NullValueHandling.Ignore)]
+        public string PillowSize { get; set; }
+
+        [JsonProperty("productDetails")]
+        public List<string> ProductDetails { get; set; }
+
+        [JsonProperty("seatCount")]
+        public string SeatCount { get; set; }
+
+        [JsonProperty("seriesId")]
+        public string SeriesId { get; set; }
+
+        [JsonProperty("shippingWeightKg")]
+        public string ShippingWeightKg { get; set; }
+
+        [JsonProperty("shippingWeightLbs")]
+        public string ShippingWeightLbs { get; set; }
+
+        [JsonProperty("sku")]
+        public string Sku { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("statusDate")]
+        public string StatusDate { get; set; }
+
+        [JsonProperty("marketIntroDates")]
+        public List<string> MarketIntroDates { get; set; }
+
+        [JsonProperty("unitDepthInches")]
+        public string UnitDepthInches { get; set; }
+
+        [JsonProperty("unitDepthMm")]
+        public string UnitDepthMm { get; set; }
+
+        [JsonProperty("unitFriendlyDimensionsInches")]
+        public string UnitFriendlyDimensionsInches { get; set; }
+
+        [JsonProperty("unitFriendlyDimensionsMm")]
+        public string UnitFriendlyDimensionsMm { get; set; }
+
+        [JsonProperty("unitHeightInches")]
+        public string UnitHeightInches { get; set; }
+
+        [JsonProperty("unitHeightMm")]
+        public string UnitHeightMm { get; set; }
+
+        [JsonProperty("unitWidthInches")]
+        public string UnitWidthInches { get; set; }
+
+        [JsonProperty("unitWidthMm")]
+        public string UnitWidthMm { get; set; }
+
+        [JsonProperty("upc")]
+        public string Upc { get; set; }
+
+        [JsonProperty("vendorName")]
+        public string VendorName { get; set; }
+
+        [JsonProperty("isExclusive")]
+        public bool IsExclusive { get; set; }
+
+        [JsonProperty("retailType")]
+        public string RetailType { get; set; }
+
+        [JsonProperty("consumerDescription")]
+        public string ConsumerDescription { get; set; }
+
+        [JsonProperty("consumerAssembly", NullValueHandling = NullValueHandling.Ignore)]
+        public string ConsumerAssembly { get; set; }
+
+        [JsonProperty("assemblyInstructionsUrl", NullValueHandling = NullValueHandling.Ignore)]
+        public Uri AssemblyInstructionsUrl { get; set; }
+
+        [JsonProperty("colorSwatch", NullValueHandling = NullValueHandling.Ignore)]
+        public Uri ColorSwatch { get; set; }
+
+        [JsonProperty("fabric", NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> Fabric { get; set; }
+
+        [JsonProperty("shade", NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> Shade { get; set; }
+
+        [JsonProperty("upholsteryFeature", NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> UpholsteryFeature { get; set; }
+    }
+
+    public class AdditionalDimension
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("alternateDimensionMm")]
+        public string AlternateDimensionMm { get; set; }
+
+        [JsonProperty("alternateDimensionInches")]
+        public string AlternateDimensionInches { get; set; }
+
+        [JsonProperty("widthMm")]
+        public string WidthMm { get; set; }
+
+        [JsonProperty("widthInches")]
+        public string WidthInches { get; set; }
+
+        [JsonProperty("heightMm")]
+        public string HeightMm { get; set; }
+
+        [JsonProperty("heightInches")]
+        public string HeightInches { get; set; }
+
+        [JsonProperty("depthMm")]
+        public string DepthMm { get; set; }
+
+        [JsonProperty("depthInches")]
+        public string DepthInches { get; set; }
+    }
+
+    public class Cover
+    {
+        [JsonProperty("itemNumber")]
+        public string ItemNumber { get; set; }
+
+        [JsonProperty("cleaningCode")]
+        public string CleaningCode { get; set; }
+
+        [JsonProperty("cleaningCodeDescription")]
+        public string CleaningCodeDescription { get; set; }
+
+        [JsonProperty("contents")]
+        public List<Content> Contents { get; set; }
+
+        [JsonProperty("location")]
+        public string Location { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("percentageMakeup")]
+        public string PercentageMakeup { get; set; }
+    }
+
+    public class Content
+    {
+        [JsonProperty("contentTypeDescription")]
+        public string ContentTypeDescription { get; set; }
+
+        [JsonProperty("percentage")]
+        public string Percentage { get; set; }
+    }
+
+    public class ImageSet
+    {
+        [JsonProperty("href")]
+        public Uri Href { get; set; }
+    }
+
+    public class Link
+    {
+        [JsonProperty("rel")]
+        public string Rel { get; set; }
+
+        [JsonProperty("href")]
+        public string Href { get; set; }
+    }
+
+    public class Metadata
+    {
+        [JsonProperty("totalRecords")]
+        public string TotalRecords { get; set; }
+
+        [JsonProperty("currentPageRecords")]
+        public string CurrentPageRecords { get; set; }
+    }
+}
---
+++ QueryProducts-NET472/Program.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using QueryProducts_NET472.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryProducts_NET472
+{
+    class Program
+    {
+        //Create an HTTP Client and enable Compression
+        static readonly HttpClient Client = new HttpClient(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
+
+        //These are your specific configuration values that you need to populate
+        const string ClientId = "API Key";
+        const string Customer = "Ashley Wholesale Customer Account Number";
+        const string ShipTo = "Ashley Ship-To related to the Customer Number";
+        const string AshleyDirectUsername = "Ashley Direct Username";
+        const string AshleyDirectPassword = "Ashley Direct Password";
+        const string PageLimit = "500";
+
+        //These are the initial connection properties for the Product Syndication API
+        const string BaseUrl = "https://apigw3.ashleyfurniture.com/productinformation/";
+        static string Path = $"products?customer={Customer}&shipto={ShipTo}&limit={PageLimit}";
+
+        static async Task Main()
+        {
+            Client.BaseAddress = new Uri(BaseUrl);
+            Client.DefaultRequestHeaders.TryAddWithoutValidation("client_id", ClientId);
+
+            //Converting the username and password to a Base64 encoded byte array
+            var byteArray = Encoding.ASCII.GetBytes($"{AshleyDirectUsername}:{AshleyDirectPassword}");
+            Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            Console.Out.WriteLine($"Starting requests for Ashley Customer {Customer}-{ShipTo}");
+
+            //For the sake of a simple implementation, we will kick off the asynchronous GetAllProducts method and await the result.
+            var items = await GetAllProducts();
+
+            foreach(var i in items)
+            {
+                //You would now do something with this data -- write it to a file, write it to a database, send it to another API, etc.
+            }
+
+            Console.Out.WriteLine("Please press any key to close");
+            Console.ReadKey();
+        }
+
+        private static async Task<List<Entity>> GetAllProducts()
+        {
+            Console.Out.WriteLine("Starting Download");
+            var items = new List<Entity>();
+
+            do
+            {
+                await Client.GetAsync(Path)
+                    .ContinueWith(async (itemSearchTask) =>
+                    {
+                        var response = await itemSearchTask;
+                        if (response.IsSuccessStatusCode) //If we get back an HTTP 200 response, start deserializing it with Newtonsoft JSON
+                        {
+                            string jsonString = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<Products>(jsonString);
+
+                            if (result != null)
+                            {
+                                // Add this page of items to the full list that we will return after the loop
+                                if (result.Entities.Any())
+                                    items.AddRange(result.Entities.ToList());
+
+                                Console.Out.WriteLine($"{items.Count} / {result.Metadata.TotalRecords} SKUs Downloaded");
+
+                                // Get the URL for the next page, if it exists, otherwise clear out our Path variable which will end our loop
+                                Path = result.Links.Where(x => x.Rel == "Next").Select(x => x.Href).FirstOrDefault() ?? string.Empty;
+                            }
+                        }
+                        else
+                        {
+                            // End loop since we had some kind of error
+                            Console.Out.WriteLine($"Error: {response.ReasonPhrase}");
+                            Path = string.Empty;
+                            
+                        }
+                    });
+
+            } while (!string.IsNullOrEmpty(Path));
+            return items;
+        }
+    }
+}
